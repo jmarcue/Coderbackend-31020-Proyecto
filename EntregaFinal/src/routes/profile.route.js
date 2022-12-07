ad4acae1afff6d5aa1a47e38d933b2a360c421bc
jmarcue <50225375+jmarcue@users.noreply.github.com>
2022-12-07 02:02:09 -0300

    Delete EntregaFinal directory

--- EntregaFinal/src/routes/profile.route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { profileController } from '../controllers/user.controller.js';
-
-const router = express.Router();
-
-router.get('/', profileController);
-
-export default router;
